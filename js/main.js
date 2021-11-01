60c3c901682edcf2d5cd5b547ccab7d0571f8cb7
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-11-01 15:11:29 +0900

    worksitayo-

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $('.gnav__link').hover(function () {
-    $(this).css('text-shadow', '0px 0px 4px #9cfffa');
+    $(this).css('text-shadow', '0px 0px 20px #38b2ff');
+}, function () {
+    $(this).css('text-shadow', 'none');
+});
+
+$('.worknav__link').hover(function () {
+    $(this).css('text-shadow', '0px 0px 20px #38b2ff');
 }, function () {
     $(this).css('text-shadow', 'none');
 });
