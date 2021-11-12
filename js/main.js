2e639c38c39dbe645ea8ea028c9213e1bbca55a5
raincoat05 <kaito_19n1100109@nnn.ed.jp>
2021-11-12 12:20:04 +0900

    ホバーアニメーションのcss化

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$('.gnav__link').hover(function () {
-    $(this).css('text-shadow', '0px 0px 20px #38b2ff');
-}, function () {
-    $(this).css('text-shadow', 'none');
-});
-
-$('.worknav__link').hover(function () {
-    $(this).css('text-shadow', '0px 0px 20px #38b2ff');
-}, function () {
-    $(this).css('text-shadow', 'none');
-});
-
 $(function(){
     setTimeout(() => {
         $('.opening').fadeOut();
