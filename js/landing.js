55655221884b9d4fb8415978cb3ff41fc6cd2c25
yuyukichi <yuukichi777@outlook.jp>
2021-12-05 00:06:41 +0900

    musicを実装

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,4 @@
+const deftxt = document.querySelector('.textbox__textbox#default').innerHTML;
 $('.gnav__link').hover(function () {
     $(this).css('text-shadow', '0px 0px 4px #9cfffa');
 }, function () {
@@ -6,21 +7,17 @@ $('.gnav__link').hover(function () {
 });
 function openDescription(target) {
     let ii;
-    if(document.querySelector('.textbox__textbox#'+target).classList.length !== 2) {
-        for(ii= 0; ii < document.querySelector('#content_list').querySelectorAll('i').length; ii++) {
-            document.querySelector('#content_list').querySelectorAll('.list__item')[ii].classList.remove('-open');
-            document.querySelectorAll('.textbox__textbox')[ii].classList.remove('-open');
+    if(document.querySelector('.list__item#'+target).classList.length !== 2) {
+            document.querySelector('table').querySelector('.list__item.-open').classList.remove('-open');
+            document.querySelector('.textbox__text').innerHTML = deftxt;
+        if(document.querySelector('table').querySelector('.list__item#'+target).classList.length !== 2){
+            document.querySelector('table').querySelector('.list__item#'+target).classList.add('-open');
+            document.querySelector('.textbox__text').innerHTML = document.querySelector('.textbox__textbox#'+target).innerHTML;
         }
-        if(document.querySelector('#content_list').querySelector('.list__item#'+target).classList.length !== 2){
-            document.querySelector('#content_list').querySelector('.list__item#'+target).classList.add('-open');
-            document.querySelector('.textbox__textbox#'+target).classList.add('-open');
-        }
-        document.querySelector('.textbox__text').classList.add('-hide');
+        // document.querySelector('.textbox__text').classList.add('-hide');
     } else {
-        for(ii= 0; ii < document.querySelector('#content_list').querySelectorAll('i').length; ii++) {
-            document.querySelector('#content_list').querySelectorAll('.list__item')[ii].classList.remove('-open');
-            document.querySelectorAll('.textbox__textbox')[ii].classList.remove('-open');
-            document.querySelector('.textbox__text').classList.remove('-hide');
-        }
+            document.querySelector('table').querySelector('.list__item.-open').classList.remove('-open');
+            document.querySelector('#default').classList.add('-open');
+            document.querySelector('.textbox__text').innerHTML = deftxt;
     }
 }
